49c5db0476f43eef5bdba1e3691aacf0367e564a
Rick1196 <ricardompp11@gmail.com>
2020-06-04 19:32:32 -0500

    test changed

--- test/examples.test.ts
+++ test/examples.test.ts
@@ -12,13 +12,13 @@ afterAll(done => {
 
 describe("Authentication API", () => {
   //body request malformed
-  it("Login API request must fail", async () => {
+  it("Login API request must fail with 400", async () => {
     let res = await request(Server).post("/api/v1/auth/login")
       .send({ username: "rick11", password: "1234" })
     expect(res.status).toEqual(400)
   })
   //must return 401, unauthorized, wrong credentials
-  it("Login API request must fail", async () => {
+  it("Login API request must fail with 401", async () => {
     let res = await request(Server).post("/api/v1/auth/login")
       .send({ user_name: "rick11", password: "1234" })
     expect(res.status).toEqual(401);
