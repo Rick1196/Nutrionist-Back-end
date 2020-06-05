053b9828349ab009c3bff87c3d4e4ddafdf8d247
Rick1196 <ricardompp11@gmail.com>
2020-06-04 21:04:35 -0500

    test promises

--- test/examples.test.ts
+++ test/examples.test.ts
@@ -1,20 +1,24 @@
 import Server from "../server";
 import request from "supertest";
-import mongoose from 'mongoose';
+
 
 
 describe("Authentication API", () => {
   //body request malformed
-  it("Login API request must fail with 400", async (done) => {
-    let res = await request(Server).post("/api/v1/auth/login")
-      .send({ username: "rick11", password: "1234" })
-    expect(res.status).toEqual(400);
+  it("Login API request must fail with 400", async () => {
+    request(Server).post("/api/v1/auth/login")
+      .send({ username: "rick11", password: "1234" }).then(res => {
+        expect(res.status).toEqual(400)
+      })
   });
   //must return 401, unauthorized, wrong credentials
-  it("Login API request must fail with 401", async (done) => {
-    let res = await request(Server).post("/api/v1/auth/login")
-      .send({ user_name: "rick11", password: "1234" })
-    expect(res.status).toEqual(401);
-    done();
+  it("Login API request must fail with 401", async () => {
+    request(Server).post("/api/v1/auth/login")
+      .send({ user_name: "rick11", password: "1234" }).then(res => {
+        expect(res.status).toEqual(401);
+
+      })
   })
+
+
 });
